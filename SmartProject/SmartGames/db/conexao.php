9586f97a06f7fc0f62a768339ed0f427846e15be
Jean Cigoli <43609958+JeanCigoli@users.noreply.github.com>
2020-08-07 11:40:05 -0300

    Update conexao.php

--- SmartProject/SmartGames/db/conexao.php
+++ SmartProject/SmartGames/db/conexao.php
@@ -4,7 +4,7 @@
 
         $host = (string) "localhost"; // local onde o bd está instalado
         $user = (string) "root"; // usuário do bd
-        $password = (string) "bcd127"; // senha do bd
+        $password = (string) ""; // senha do bd
         $database = (string) "db_smartgames"; // o nome do database
 
         $conexao = mysqli_connect($host, $user, $password, $database);
